4fd0602448544e652b348cc42da2332ff69717e5
francids <francisco.mc.ids@gmail.com>
2025-02-12 14:56:27 -0400

    style: Remove `main.css` and add global styles in `index.html`

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 
-import "./styles/main.css";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 
